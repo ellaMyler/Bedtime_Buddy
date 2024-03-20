4f5f431679fae642a73c5ee216c5d25523a72830
haffe017 <haffe017@umn.edu>
2024-03-20 16:50:04 -0500

    Stats Page Update!
    Combined the stats page into the test main!

--- lib/stats_page.dart
+++ lib/stats_page.dart
@@ -6,9 +6,43 @@ class SleepStatsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      // this is where everything on the page will go! Put the widgets and stuff here!
-      child: Text('Sleep Stats', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Sleep Stats', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(child:
+              Text('Goals', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(color:Colors.black),
+                ),
+              ),
+              SizedBox(height: 40),
+              const Text('Weekly Summary', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 15),
+              Container(
+                width: 250,
+                height: 250,
+                decoration: BoxDecoration(
+                  border: Border.all(color:Colors.black),
+                ),
+              ),
+            ]
+        )
     );
   }
 }
+
+//return const Column(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       children: [
+//         Text('Sleep Stats', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+//         Text('Goals', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+//       ],
