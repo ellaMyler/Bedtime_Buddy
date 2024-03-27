b9ab74c970ac1e0e076d8cbd821c0fdf2d3f9f8f
Ella Myler <myler021@umn.edu>
2024-03-27 16:09:31 -0500

    Updated box sizes
    also added a feature to scroll on the page

--- lib/stats_page.dart
+++ lib/stats_page.dart
@@ -10,33 +10,45 @@ class SleepStatsPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Sleep Stats', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
         ),
-        body: Column(
+        body: SingleChildScrollView(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Center(child:
-              Text('Goals', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-              ),
+                Text('(to be made in future sprint)', style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+    ),
               SizedBox(height: 15),
               Container(
                 width: 250,
-                height: 250,
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(color:Colors.black),
+                ),
+                child: Text('(graph of weekly sleep stats, with amount of sleep shown)', style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal) ),
+              ),
+              SizedBox(height: 20),
+                Text('Goals', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              SizedBox(height: 15),
+              Container(
+                width: 250,
+                height: 200,
                 decoration: BoxDecoration(
                   border: Border.all(color:Colors.black),
                 ),
               ),
-              SizedBox(height: 40),
+              SizedBox(height: 20),
               const Text('Weekly Summary', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               SizedBox(height: 15),
               Container(
                 width: 250,
-                height: 250,
+                height: 200,
                 decoration: BoxDecoration(
                   border: Border.all(color:Colors.black),
                 ),
               ),
             ]
         )
-    );
+    ));
   }
 }
 
