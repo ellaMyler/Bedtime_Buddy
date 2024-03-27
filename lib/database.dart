f27f13d8a034cd1bb00d6415022c22e25dbb90fb
Todd Sjodin <sjodi048@d.umn.edu>
2024-03-27 15:51:11 -0500

    Update database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -4,19 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 
 final databaseReference = FirebaseDatabase.instance.ref();
 
-<<<<<<< Updated upstream
+
 void sendBedtime(String type, String dayOfWeek, String timeOfDay) async {
-  databaseReference.push().set({'Type':type, 'Day of Week':dayOfWeek, 'Time of Day':timeOfDay});
-=======
-
-
-
-
-
-
-
+  databaseReference.push().set(
+      {'Type': type, 'Day of Week': dayOfWeek, 'Time of Day': timeOfDay});
+}
 
 void sendMessage (String message) async {
   databaseReference.push().set({'Logged Sleep':message});
->>>>>>> Stashed changes
+
 }
