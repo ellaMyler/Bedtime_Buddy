f0bec0308c1bf52ba80dc463f5ad0c6e0be9ecce
Corey Tessman <tessm089@d.umn.edu>
2024-04-09 14:58:11 -0500

    working

--- lib/database.dart
+++ lib/database.dart
@@ -10,7 +10,28 @@ void sendBedtime(String type, String dayOfWeek, String timeOfDay) async {
       {'Type': type, 'Day of Week': dayOfWeek, 'Time of Day': timeOfDay});
 }
 
-void sendMessage (String message) async {
-  databaseReference.push().set({'Logged Sleep':message});
+void sendSleepLog (String dateLogged, String sleepThoughts, String dreamNight, String sleepQuality, String stressLevel, int hours, int minutes) async {
+  String sanitizedKey = dateLogged.replaceAll(RegExp(r'[#$/\[\]]'), ''); //Removes any illegal characters
+  //Makes it so data is saved in the same places
+  Map<String, dynamic> sleepLogData = {
+    'Date Logged': dateLogged,
+    'Thoughts on your sleep': sleepThoughts,
+    'Your dreams were': dreamNight,
+    'Your quality of sleep was': sleepQuality,
+    'Your level of stress that night was': stressLevel,
+    'Hours slept': hours,
+    'Minutes slept': minutes,
+  };
 
+  await databaseReference.child(sanitizedKey).set(sleepLogData);
+}
+
+void readData() async {
+  final ref = FirebaseDatabase.instance.ref();
+  final snapshot = await ref.child('April 4, 2024/message').get();
+  if (snapshot.exists){
+    print(snapshot.value);
+  } else {
+    print('No data available');
+  }
 }
