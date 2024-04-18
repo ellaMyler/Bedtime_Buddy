7ac697302f00a37667d5c9dd53819eb4d0f29f58
haffe017 <haffe017@umn.edu>
2024-04-18 13:17:37 -0500

    osidjhfsdfji

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,34 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:app_settings/app_settings.dart';
-import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/rendering.dart';
 import 'notification_controller.dart';
 import 'notification_maker.dart';
+import 'dart:math' as math;
 
-class SettingsPage extends StatelessWidget {
+
+class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  List<String> imagePaths = [
+    'lib/assets/thumbnails/white.jpg', // Image path for switch0
+    'lib/assets/thumbnails/notifications.png',  // Image path for switch1
+    'lib/assets/thumbnails/white.jpg',
+    'lib/assets/thumbnails/white.jpg',
+    'lib/assets/thumbnails/off.png',
+    'lib/assets/thumbnails/white.jpg',
+    'lib/assets/thumbnails/white.jpg',
+    'lib/assets/thumbnails/off.png',
+    'lib/assets/thumbnails/white.jpg',
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    double _crossAxisSpacing = 0, _mainAxisSpacing = 12, _aspectRatio = 2.5;
+    int _crossAxisCount = 3;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    var width = (screenWidth - ((_crossAxisCount - 1) * _crossAxisSpacing)) / _crossAxisCount;
+    var height = width / _aspectRatio;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-           const ElevatedButton(
-              onPressed:NotificationService.showTestNotification,
-              child:  Text('Send Test Notification'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to App Settings for permission adjustment
+      body: GridView.builder(
+        padding: EdgeInsets.fromLTRB(0, height * 3, 0, 0),
+        itemCount: 9,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+              // Toggle the image path for the tapped item
+              switch (index) {
+              case 1:
+                setState(() {
+                  imagePaths[index] = imagePaths[index] == 'lib/assets/thumbnails/notifications.png'
+                      ? 'lib/assets/thumbnails/notifications.png'
+                      : 'lib/assets/thumbnails/notifications.png';});
                 AppSettings.openAppSettings();
-              },
-              child: const Text('Open App Settings'),
+                break;
+              case 4:
+                setState(() {
+                  imagePaths[index] = imagePaths[index] == 'lib/assets/thumbnails/off.png'
+                      ? 'lib/assets/thumbnails/on.png'
+                      : 'lib/assets/thumbnails/off.png';});
+                break;
+              case 7:
+                setState(() {
+                  imagePaths[index] = imagePaths[index] == 'lib/assets/thumbnails/off.png'
+                      ? 'lib/assets/thumbnails/on.png'
+                      : 'lib/assets/thumbnails/off.png';});
+                break;
+          }},
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePaths[index]), // Use the image path based on the index
+                fit: BoxFit.cover,
+              ),
             ),
-          ],
+            // You can customize the container further if needed
+          ),
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: _crossAxisCount,
+          crossAxisSpacing: _crossAxisSpacing,
+          mainAxisSpacing: _mainAxisSpacing,
+          childAspectRatio: _aspectRatio,
         ),
       ),
     );
